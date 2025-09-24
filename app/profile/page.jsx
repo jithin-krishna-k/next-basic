97a0d8c63d31d277036d002c18af40f36a3a4611
jithin krishna k <jithinkrishna526@gmail.com>
2025-09-24 17:40:05 +0530

    authcontext added

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+'use client'
 
-const Profile = () => {
+import { useAuth } from '../context/AuthContext'
+
+export default function Profile() {
+  const { user, login, logout } = useAuth()
+
   return (
-    <div>Profile</div>
+    <div>
+      <h1>Hello, {user ? user.name : 'Guest'}</h1>
+      {!user && <button onClick={() => login({ name: 'John' })}>Login</button>}
+      {user && <button onClick={logout}>Logout</button>}
+    </div>
   )
 }
-
-export default Profile
